a3c28aff8577942ac8e4332c0dedf0449fbe1f4c
Kitazii <mr.kieranburns@gmail.com>
2025-01-05 20:35:15 +0000

    Context Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { UseProvider } from "./Context/userAuth";
 
 function App() {
   return (
     <>
+    <UseProvider>
       <Navbar />
       <Outlet />
       <ToastContainer />
+    </UseProvider>
     </>
   );
 }
