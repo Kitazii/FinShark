2cd63673e494a60587e1e3e41cd4dfbf2aab0dff
Kitazii <mr.kieranburns@gmail.com>
2025-01-12 19:16:44 +0000

    Added a Comment Box

--- frontend/src/Components/CompanyProfile/CompanyProfile.tsx
+++ frontend/src/Components/CompanyProfile/CompanyProfile.tsx
@@ -8,6 +8,7 @@ import {
   formatLargeNonMonetaryNumber,
   formatRatio,
 } from "../../Helpers/NumberFormatting";
+import StockComment from "../StockComment/StockComment";
 
 interface Props {}
 
@@ -95,6 +96,7 @@ const CompanyProfile = (props: Props) => {
       {companyData ? (
         <>
           <RatioList data={companyData} config={tableConfig} />
+          <StockComment stockSymbol={ticker} />
         </>
       ) : (
         <Spinner />
