fc9b313a711f128e3b4c7e7cc47092b2fcf09cff
Kitazii <mr.kieranburns@gmail.com>
2025-01-14 01:11:00 +0000

    Comment List addedd

--- frontend/src/Models/Comment.ts
+++ frontend/src/Models/Comment.ts
@@ -1,4 +1,15 @@
+export type AppUser = {
+    id: string;
+    userName: string;
+  };
+
 export type CommentPost = {
     title: string;
     content: string;
+};
+
+export type CommentGet = {
+    title: string;
+    content: string;
+    appUser: AppUser
 };
--- frontend/src/Services/CommentService.tsx
+++ frontend/src/Services/CommentService.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CommentPost } from "../Models/Comment";
+import { CommentGet, CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ErrorHandler";
 
 const api = "http://localhost:5051/api/comment/";
@@ -15,4 +15,13 @@ export const commentPostAPI = async(title: string, content: string, symbol: stri
     } catch (error) {
         handleError(error);
     }
-}
+};
+
+export const commentGetAPI = async(symbol: string) => {
+    try {
+        const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
